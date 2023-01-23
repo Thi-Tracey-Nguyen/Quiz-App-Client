cd36c0d55bd91b2245efaf7a980939e2b0ca53ca
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-01-23 22:19:13 +1100

    tested categories route using open trivia api

--- src/categories/Categories.jsx
+++ src/categories/Categories.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Categories = () => {
+
+  const [ categories, setCategories ] = useState([])
+
+  useEffect(() => {
+    async function fetchAPI() {
+      const res = await fetch('https://opentdb.com/api.php?amount=10')
+      const data = await res.json()
+      const questions = data.results
+      const cat = []
+      for (const question of questions) {
+        cat.push(question.category)
+      }
+      setCategories(cat)
+    }
+    fetchAPI()
+  }, [])
+
   return (
-    <div>This page shows all the categories</div>
+    <>
+      <h2>Categories</h2>
+      {categories.map((category, index) => <p key={index}>{category}</p>)}
+    </>
   )
 }
 
