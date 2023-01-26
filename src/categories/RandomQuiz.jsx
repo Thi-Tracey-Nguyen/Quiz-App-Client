7b6b1c6815f93694624f95e7daf03283676df4b7
Essteel <siannon.steel@gmail.com>
2023-01-26 14:50:01 +1030

    Created RandomQuiz component, fixed issue with Quizzes and RandomCat Links

--- src/categories/RandomQuiz.jsx
+++ src/categories/RandomQuiz.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const RandomCat = ({ categories }) => {
+const RandomQuiz = ({ quizzes }) => {
 
-  // function to get a random category from the categories array
-  function randomCat(array) { 
+  // function to get a random quiz from the quizzes array
+  function randomQuiz(array) { 
     const index = Math.floor(Math.random()*(array.length)) //generate a random index
     return array[index]
   }
 
-  const category = randomCat(categories)
+  const quiz = randomQuiz(quizzes)
 
   return (
     <>
       <p>Can't choose?</p>
       <button>
-        <Link to={`/categories/${category.name}`}>
-          Random Category
+        <Link to={`/quizzes/${quiz._id}`}>
+          Random Quiz
         </Link>
       </button>
     </>
   )
 }
 
-export default RandomCat
+export default RandomQuiz
