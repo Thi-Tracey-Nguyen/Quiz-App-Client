d7c588b51fbaeb27144004f8c0db1e562a48a63f
Essteel <siannon.steel@gmail.com>
2023-01-26 11:09:14 +1030

    Added Footer to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './home/Home'
 import NavBar from './home/NavBar'
+import Footer from './home/Footer'
 import Categories from './categories/Categories'
 import Leaderboard from './leaderboard/Leaderboard'
 import LogIn from './login/LogIn'
@@ -47,6 +48,7 @@ const App = () => {
           <Route path='/profile' element={<Profile />} />
           <Route path='*' element={<h4>Page not found</h4>} />
         </Routes>
+      <Footer />
     </>
   )
 }
