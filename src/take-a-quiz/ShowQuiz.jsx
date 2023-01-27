b048430e536fdee748ebbd1f1f2f1265473aadfa
Essteel <siannon.steel@gmail.com>
2023-01-27 15:56:31 +1030

    Updated ShowQuiz route and RandomQuiz to fix routing issue

--- src/take-a-quiz/ShowQuiz.jsx
+++ src/take-a-quiz/ShowQuiz.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ShowQuiz = ({ quiz }) => {
-  return (
+	return (
     <>
       <img src={quiz.image} alt='Alt text for quiz image'/>
 			<br />
-			<Link to={`quizzes/${quiz._id}`}>{quiz.title}</Link>
+			<Link to={`/quizzes/${quiz._id}`}>{quiz.title}</Link>
     	<p>Author: {quiz.author} <br />
     	{quiz.questions.length} questions</p>
     </>
