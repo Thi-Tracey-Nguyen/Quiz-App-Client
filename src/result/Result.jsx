dbad29a2eca81205a95fd88b89b760ddc6f95f67
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-01-28 11:43:34 +1100

    Implemented calculate points for result page

--- src/result/Result.jsx
+++ src/result/Result.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Result = ({ answers, quiz }) => {
+
+  let points = 0
+
+  function calculatePoints(answers, quiz) {
+    for (let i = 0; i < answers.length; i++) {
+      if (answers[i] === quiz.questions[i].correctAnswer) {
+        points = points + 1
+      }
+  }}
+
+  calculatePoints(answers, quiz)
+
   return (
     <>
       <h1>Result Page</h1>
+      <h2>Your points: {points}</h2>
       <p>Your answers are:</p>
       {answers.map((answer, index) => <p key={index}>{answer}</p>)}
       <p>Correct answers are:</p>
