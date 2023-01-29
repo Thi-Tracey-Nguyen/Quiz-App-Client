1c134d0a674601fd73f50b950ba8c603e8a85ec6
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-01-29 13:07:58 +1100

    Fixed random quiz issue

--- src/categories/Quizzes.jsx
+++ src/categories/Quizzes.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import RandomQuiz from './RandomQuiz'
 import ShowQuiz from '../take-a-quiz/ShowQuiz'
 import ReturnToTop from '../UI/ReturnToTop'
 
 const Quizzes = ({ quizzes }) => {
+
   return (
     <>
       <h1>All Quizzes</h1>
-      <ul>
-        {quizzes.length === 0 ? 'Loading...' : quizzes.map((quiz, index) => (
-          <div key={index}>
-            <ShowQuiz quiz={quiz}/>
-          </div>
-        ))}
-        <RandomQuiz quizzes={quizzes}/>
-      </ul>
+      {quizzes.length === 0 ? 'Loading...' : quizzes.map((quiz, index) => (
+        <div key={index}>
+          <ShowQuiz quiz={quiz}/>
+        </div>
+      ))}
+      <RandomQuiz quizzes={quizzes} />
       <ReturnToTop />
     </>
   )
