310ebba44c10d4aa0aba969527588fdfd06d0a8c
Steffy Johnson <steffy.johnson@Steffys-MacBook-Pro.local>
2023-01-31 14:58:04 +1100

    Adding Delete quiz feature.

--- src/edit-a-quiz/EditQuizzes.jsx
+++ src/edit-a-quiz/EditQuizzes.jsx
@@ -1,22 +1,115 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ShowQuiz from '../take-a-quiz/ShowQuiz'
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import ShowQuiz from "../take-a-quiz/ShowQuiz"
+import AddQuestionsForm from "../make-a-quiz/AddQuestionsForm"
+
 
 const EditQuizzes = ({ quizzes }) => {
+  const [selectedQuiz, setSelectedQuiz] = useState(null)
+  const [showConfirm, setShowConfirm] = useState(false)
+  const [selectedQuizEdit, setSelectedQuizEdit] = useState(null)
+  const [showConfirmEdit, setShowConfirmEdit] = useState(false)
+    
+  const navigateTo = useNavigate()
+
+  const handleConfirmDelete = () => {
+    console.log('calling handleDeleteQuiz :', selectedQuiz._id)
+    handleDeleteQuiz(selectedQuiz._id)
+    setSelectedQuiz(null)        
+    setShowConfirm(true) 
+    navigateTo('/edit-a-quiz/')
+  }
+
+  const handleDeleteQuiz = async (id) => {
+    console.log('Deleting quiz:', id)
+    try {
+      await fetch(
+        `https://quiz-app-server-production-09e8.up.railway.app/quizzes/${id}`,
+        {
+          method: "DELETE",
+        }
+      )
+      // console.log('Quiz deleted:', title)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const handleCloseConfirm = () => {
+    setShowConfirm(false)
+    navigateTo('/edit-a-quiz/')
+  }
+
+  const handleConfirmEdit = () => {
+    console.log('calling handleEditQuiz :', selectedQuizEdit._id)    
+    //return <AddQuestionsForm quizId={selectedQuizEdit._id} />
+    //setSelectedQuizEdit(null)    
+    //setShowConfirmEdit(true) 
+    //navigateTo('/edit-a-quiz/')
+  }
+  const handleEditQuiz = async (id) => {
+    console.log('Edit quiz:', id)
+    try {
+
+      // console.log('Quiz deleted:', title)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  const handleCloseConfirmEdit = () => {
+    setShowConfirmEdit(false)
+    navigateTo('/edit-a-quiz/')
+  }
+  
+
   return (
     <>
       <h1>Choose a Quiz to edit or delete</h1>
-      <ul> 
-          {quizzes.length === 0 ? 'Loading...' : quizzes.map((quiz, index) => (
+      <ul>
+        {quizzes.length === 0
+          ? "Loading..."
+          : quizzes.map((quiz, index) => (
               <div key={index}>
-                <ShowQuiz quiz={quiz}/>
-                <ul>
-                    <Link to={`quizzes/${quiz.title}/edit`}>Edit</Link>
-                    <Link to={`quizzes/${quiz.title}/delete`}>Delete</Link>
+                <ShowQuiz quiz={quiz} />
+                <ul>                  
+                  <button onClick={() => setSelectedQuizEdit(quiz)}>Edit</button>
+                  <button onClick={() => setSelectedQuiz(quiz)}>Delete</button>
                 </ul>
               </div>
-          ))}
-        </ul>
+            ))}
+      </ul>
+      {selectedQuizEdit && (
+        <div>
+          <p>
+            Are you sure you want to edit the quiz "{selectedQuizEdit.title}"?
+          </p>
+          <button onClick={handleConfirmEdit}>Confirm</button>
+          <button onClick={() => setSelectedQuizEdit(null)}>Cancel</button>
+        </div>
+      )}
+      {showConfirmEdit && (
+        <div>
+          <p>Quiz Editted successfully!</p>
+          <button onClick={handleCloseConfirmEdit}>Close</button>
+        </div>
+      )}            
+      {selectedQuiz && (
+        <div>
+          <p>
+            Are you sure you want to delete the quiz "{selectedQuiz.title}"?
+          </p>
+          <button onClick={handleConfirmDelete}>Confirm</button>
+          <button onClick={() => setSelectedQuiz(null)}>Cancel</button>
+        </div>
+      )}
+      {showConfirm && (
+        <div>
+          <p>Quiz deleted successfully!</p>
+          <button onClick={handleCloseConfirm}>Close</button>
+        </div>
+      )}
+      
     </>
   )
 }
