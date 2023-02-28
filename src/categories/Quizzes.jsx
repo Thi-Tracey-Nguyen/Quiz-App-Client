698a0d120444476e93a968eec374995c64e98305
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-02-28 14:59:04 +1100

    implemented hovering effect for showquiz

--- src/categories/Quizzes.jsx
+++ src/categories/Quizzes.jsx
@@ -18,7 +18,7 @@ const Quizzes = ({ quizzes, setQuizzes, questions }) => {
     <>
       <div className="main-body flex-wrap" style={{ height: "100vh" }}>
         <h1>All Quizzes</h1>
-          <ul className="d-flex justify-content-center flex-wrap ">
+          <ul className="d-flex justify-content-center flex-wrap">
             {isNaN(quizzes) ? quizzes.map((quiz, index) => (
                 <div key={index} data-testid='quiz' className="card m-3" style={{ width: "15rem" }} >
                   <ShowQuiz quiz={quiz} />
--- src/categories/ShowQuiz.jsx
+++ src/categories/ShowQuiz.jsx
@@ -3,22 +3,26 @@ import { Link } from "react-router-dom";
 
 const ShowQuiz = ({ quiz }) => {
   return (
-    <>
-      <div className="d-flex justify-content-center flex-wrap">
+    <div className="d-flex justify-content-center flex-wrap">
+      <div className="container">
         <img
           src={quiz.image}
           alt="Alt text for quiz image"
           className="img-fixed-size"
           style={{ width: "15rem", height: "15rem" }}
         />
-        <Link
-          to={`/quizzes/${quiz._id}`}
-          className="card m-3 align-items-center text-dark fw-bold"
+        
+        <p
+          className="m-3 align-items-center text-dark fw-bold title"
           style={{ width: "15rem" }}
         >
           {quiz.title}
-        </Link>
-        <div className="card m-3" style={{ width: "15rem" }}>
+        </p>
+        
+        <Link
+          to={`/quizzes/${quiz._id}`}
+          className="content"
+        >
           <div className="fw-bold">Author:</div>
           <div>{quiz.author}</div>
           <div className="fw-bold">Number of Questions:</div>
@@ -27,9 +31,9 @@ const ShowQuiz = ({ quiz }) => {
           ) : (
             <div>{quiz.questions.length} questions</div>
           )}
-        </div>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 export default ShowQuiz;
